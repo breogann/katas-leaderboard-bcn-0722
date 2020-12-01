f53d2ae088b8c87d6ac440d732a64326aa68725a
Ras Soyungalgo <agalvezcorell@gmail.com>
2020-12-01 18:12:44 +0100

    FIXIIIIIIING

--- katas.py
+++ katas.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as n
 import os
 import pandas as pd
 from PIL import Image
@@ -15,21 +16,38 @@ st.image (image,use_column_width=True)
 os.system("curl -LJO https://raw.githubusercontent.com/agalvezcorell/codewars-kata-student-correction-ih/master/output/output.csv")
 
 katas = pd.read_csv("output.csv")
-kyu = [5, 6, 5, 5, 5, 5, 5, 6]
-katas_ponderadas = {j:k for j,k in zip(katas.columns, kyu)}
+kyu = [5, 7, 6, 5, 6, 5, 6, 6, 7, 6, 6]
+pondera = {5:12, 6:9, 7:6, 8:4}
+puntos = [pondera.get(a) for a in kyu]
+
 
 katas.set_index('username', inplace=True)
-katas = katas[katas.columns[11:]]
-katas = katas.applymap(lambda x: 1 if x==True else 0)
+listadrop = ['counting-sheep-dot-dot-dot',
+ 'fake-binary',
+ 'sum-mixed-array',
+ 'deodorant-evaporator',
+ 'user-class-for-banking-system',
+ 'counting-in-the-amazon',
+ 'tests-results',
+ 'count-the-smiley-faces',
+ 'see-you-next-happy-year',
+ 'sql-with-harry-potter-sorting-hat-comparators',
+ 'sql-basics-monsters-using-case']
+
+katas = katas.drop(listadrop, axis = 1)
+#katas = katas.applymap(lambda x: 1 if x==True else 0)
+katas_ponderadas = {nombre:valor for nombre,valor in zip(katas.columns, puntos)}
     
+    
+    #Ponderación en el DF
+import numpy as np
+for columna in katas.columns:
+    katas[f"{columna}"] = np.where(katas[f"{columna}"] == True,katas_ponderadas.get(f"{columna}"),0)
+
     #Total katas
 katas['TOTAL_KATAS'] = katas.sum(axis=1)
 
-    #Ponderación
-for i in katas_ponderadas:
-    for j in katas.columns:
-        katas[j].loc[(katas[j] == 1)] = 1*katas_ponderadas[i]
-    
+
     #Suma por puntos
 katas['PUNTOS_TOTAL'] = katas.sum(axis=1) - katas.TOTAL_KATAS
 katas = katas.sort_values(by=['PUNTOS_TOTAL'], ascending=False)
@@ -48,3 +66,10 @@ Mira las katas que has hecho y los puntos que te da cada una
 """
 )
 st.dataframe(katas)
+
+total = sum(katas_ponderadas.values())
+
+st.write(
+f"El total de puntos es {total}"
+)
+
